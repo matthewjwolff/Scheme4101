79ac0f370b3b045c8e853c6ec250aef180d5bf30
Matthew Wolff <mwolff3@tigers.lsu.edu>
2015-11-12 23:41:00 -0600

    Define & set

--- Special/Define.cs
+++ Special/Define.cs
@@ -12,6 +12,24 @@ namespace Tree
         {
             Printer.printDefine(t, n, p);
         }
+
+        public override Node eval(Node t, Environment env)
+        {
+            Node variable = t.getCdr().getCar();
+            if(variable.isPair())
+            {
+                //variable is of the form (decl params...), ex: (foo x y)
+                Node declaration = variable.getCar();
+                Node parameters = variable.getCdr();
+                Cons lambda = new Cons(new Ident("lambda"),new Cons(parameters,t.getCdr().getCdr().getCar()));
+                env.define(declaration,new Closure(lambda, env));
+            }
+            else
+            {
+                env.define(variable, t.getCdr().getCdr().getCar().eval(env));
+            }
+            return new StringLit("definition recorded");
+        }
         /**
         Define:
         (define var value)
