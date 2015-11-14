56f2c9ca36f6a198ca16e567a34ae9909eb81552
Matthew Wolff <mwolff3@lsu.edu>
2015-11-13 19:02:10 -0600

    Got a better regular.eval working for car. Will find out if it works for everything soon.

--- Special/Regular.cs
+++ Special/Regular.cs
@@ -16,6 +16,15 @@ namespace Tree
         public override Node eval(Node t, Environment env)
         {
             Node car = t.getCar().eval(env);
+            Node cdr = t.getCdr().eval(env);
+            if (t.getCar().isSymbol())
+            {
+                //Will be evaluated to a procedure(?)
+                return car.apply(cdr);
+            }
+            else return new Cons(car,cdr);
+            /**
+            Node car = t.getCar().eval(env);
             //Get the argument list
             Node cdr = t.getCdr();
             //Evaluate each argument, and replace the argument with its evaluated value
@@ -25,6 +34,7 @@ namespace Tree
                 cdr = cdr.getCdr();
             }
             return car.apply(t.getCdr());
+            */
         }
         /**
             node = (something that was passed in during the constructor, figure out what it is later
